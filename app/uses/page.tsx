aaba8ac5035357ddb422d2564a57a40fc8ac943a
Evan <jihuawu118@gmail.com>
2025-04-05 14:32:04 +0800

    chore: update next config and uses page

--- app/uses/page.tsx
+++ app/uses/page.tsx
@@ -5,7 +5,7 @@ export default function Page() {
     <Container className="mt-16 sm:mt-32">
       <header className="max-w-2xl">
         <h1 className="text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
-          Coming Soon
+          Coming soon
         </h1>
         <p className="mt-6 text-lg text-zinc-600 dark:text-zinc-400">
           This page is under construction, please check back later.
--- next.config.ts
+++ next.config.ts
@@ -3,24 +3,6 @@ import type { NextConfig } from 'next'
 const nextConfig: NextConfig = {
   reactStrictMode: false,
   transpilePackages: ['next-mdx-remote'],
-  rewrites: async () => [
-    {
-      source: '/x',
-      destination: 'https://x.com/evan1297',
-    },
-    {
-      source: '/linkedin',
-      destination: 'https://www.linkedin.com/in/evan976',
-    },
-    {
-      source: '/github',
-      destination: 'https://github.com/evan976',
-    },
-    {
-      source: '/telegram',
-      destination: 'https://t.me/evan9712',
-    },
-  ],
 }
 
 export default nextConfig
