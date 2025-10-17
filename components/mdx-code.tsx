ec805137a559db994cfdbb7e990e6279aea2f595
Evan <jihuawu118@gmail.com>
2025-10-17 18:18:16 +0800

    chore: update typography

--- components/mdx-code.tsx
+++ components/mdx-code.tsx
@@ -20,15 +20,15 @@ export async function MDXCode({
   })
 
   return (
-    <div className="rounded-lg not-prose my-6 border overflow-hidden border-zinc-200 dark:border-white/10">
-      <div className="py-2 px-4 flex items-center justify-between bg-zinc-200/20 dark:bg-zinc-800/90 border-b border-zinc-200 dark:border-white/10">
-        {filename ? (
+    <div className="rounded-xl relative not-prose my-6 border overflow-hidden border-zinc-200 dark:border-white/10">
+      {filename && (
+        <div className="py-2 px-4 flex items-center justify-between bg-zinc-50 dark:bg-zinc-800 border-b border-zinc-200 dark:border-white/10">
           <span className="text-sm/5 text-zinc-500 font-mono dark:text-white/50">
             {filename}
           </span>
-        ) : null}
-        <CopyButton value={source.code} className="ml-auto" />
-      </div>
+        </div>
+      )}
+      <CopyButton value={source.code} className="absolute top-3 right-4" />
       <div
         className="*:flex *:*:max-w-none *:*:shrink-0 *:*:grow *:overflow-auto *:p-4 *:text-sm/relaxed
           **:[.line]:isolate **:[.line]:not-last:min-h-[1lh] [&_code]:font-mono"
